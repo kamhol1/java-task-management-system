275bcc975c2e404dfb4118fcce5d03dbe9fa9b40
kamhol1 <kamhol271@gmail.com>
2023-05-08 21:54:39 +0200

    First commit

---
+++ src/main/java/com/example/jtms/JtmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jtms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jtms/JtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jtms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
